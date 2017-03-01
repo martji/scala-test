a09c9f6080f2f4453a0d4ecf0bbca228e25d7667
martji <magq1001@gmail.com>
2017-03-01 21:27:40 +0800

    learn collection

--- src/main/scala/CollectionTest.scala
+++ src/main/scala/CollectionTest.scala
@@ -1,20 +1,41 @@
+import scala.collection.immutable.TreeSet
+
 /**
   * Created by Guoqing on 2016/10/20.
   */
 
-import scala.io._
+object CollectionTest extends App {
 
-object CollectionTest {
-
-    def main(args: Array[String]): Unit = {
+    override def main(args: Array[String]): Unit = {
         val x = Map("one" -> 1, "two" -> 2, "three" -> 3)
         println(x.maxBy(_._2)._1)
+        val list = x.values.flatMap(x => List(x, x)).toList
+        list.map(x => x * x).foreach(print)
 
-        val n = StdIn.readInt()
-        val arr = new Array[Int](n)
-        for (i <- 0 to n - 1) {
-            arr(i) = StdIn.readInt()
+        def inc = new PartialFunction[Any, Int] {
+            override def isDefinedAt(x: Any): Boolean = x.isInstanceOf[Int] && x.asInstanceOf[Int] > 1
+
+            override def apply(x: Any): Int = x.asInstanceOf[Int] + 1
         }
-        println(arr.sorted.toList)
+        println(list collect inc)
+
+        list.filter(_ > 2).foreach(x => print(x + " "))
+        println(list.sum)
+        println(list.toBuffer += (5, 6))
+
+        println("\n------666------\n")
+
+        val list2 = List(1, 3, -2) :+ -4
+        println(list2.sortBy(x => x * x))
+
+        val fruit = Set("apple", "orange", "peach", "banana")
+        println(fruit("apple"))
+
+        val ordering = Ordering.fromLessThan[String](_.length <= _.length)
+        println(TreeSet.empty(ordering) ++ fruit)
+
+        val vector = (2 :: list2).toVector
+        println(vector)
+
     }
 }
---
+++ src/main/scala/IOTest.scala
@@ -0,0 +1,14 @@
+import scala.io.StdIn
+
+/**
+  * Created by Guoqing on 2017/3/1.
+  */
+
+object  IOTest {
+
+    def main(args: Array[String]): Unit = {
+        val n = StdIn.readInt()
+        println(s"n = $n")
+    }
+}
+
